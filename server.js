808e2ff73d10b91a8980e35d1a6755ff7d8c8669
rodrigobatista-dev <rodrigobatista03@gmail.com>
2025-08-20 22:15:37 -0300

    Primeiro commit

---
+++ server.js
@@ -0,0 +1,58 @@
+import express from "express";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const app = express();
+app.use(express.json()); // Middleware para analisar o corpo da requisição como JSON
+app.use(cors());
+
+app.get("/usuarios", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.status(200).json(users);
+});
+
+app.post("/usuarios", async (req, res) => {
+  const user = await prisma.user.create({
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      age: req.body.age,
+    },
+  });
+
+  res.status(201).json(user);
+});
+
+app.put("/usuarios/:id", async (req, res) => {
+  const user = await prisma.user.update({
+    where: {
+      id: req.params.id,
+    },
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      age: req.body.age,
+    },
+  });
+
+  res.status(200).json(user);
+});
+
+app.delete("/usuarios/:id", async (req, res) => {
+  await prisma.user.delete({
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  res.status(200).json({ message: "Usuário deletado com sucesso" });
+});
+
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
+});
+
+//rodrigobatista03
+//NWbWEz5Ed1LZ2rGr
